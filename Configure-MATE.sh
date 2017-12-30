9f8e76f718bf746fb1733645e387b045512b03a9
Matt Mayfield <mdmayfield@users.noreply.github.com>
2017-12-30 16:50:22 -0600

    Initial commit

---
+++ Configure-MATE.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Short script to pre-configure Ubuntu MATE
+
+
+error()
+{
+    echo "${1:-"Error while running script."}" 1>&2
+    exit 1
+}
+
+
+echo "Copying panel layout configurations. Will need root password."
+sudo cp MM-Custom-tweak.layout /usr/share/layouts/ || error "Could not copy layout file."
+sudo cp MM-Custom-tweak.panel /usr/share/layouts/ || error "Could not copy panel file."
+cat dconf load / < custom.dconf || error "Loading dconf file failed."
+
+
+
+
+
+
